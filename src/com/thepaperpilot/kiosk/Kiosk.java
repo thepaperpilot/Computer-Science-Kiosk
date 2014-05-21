46cbad25c7a7cf437feb79b4b0e5d7e7e4500a0b
thepaperpilot <pi045427>
2014-05-21 09:58:09 -0500

    Approximate time taken: 145m * Made the Panel class * Made the renderer * Made a temporary testing skin/font * Made a temporary testing panel

--- src/com/thepaperpilot/kiosk/Kiosk.java
+++ src/com/thepaperpilot/kiosk/Kiosk.java
@@ -1,45 +1,105 @@
 package com.thepaperpilot.kiosk;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.thepaperpilot.kiosk.panels.Panel;
+
+import java.util.ArrayList;
 
 public class Kiosk implements ApplicationListener {
 
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.fullscreen = true;
+    // Dimensions of the tablet: DO NOT CHANGE
+    public final static int WIDTH = 1366;
+    public final static int HEIGHT = 768;
 
-		new LwjglApplication(new Kiosk(), cfg);
-	}
+    public final static int PADDING = 10;
 
-	@Override
-	public void create() {
-		System.exit(0);
-	}
+    public static Skin skin;
+    private static Panel panel;
+    private static ArrayList<Panel> panelHistory;
 
-	@Override
-	public void resize(int width, int height) {
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.width = WIDTH;
+        cfg.height = HEIGHT;
+        // Full screen is annoying while developing
+        // Make sure to uncomment this in any shippable version
+        // cfg.fullscreen = true;
 
-	}
+        new LwjglApplication(new Kiosk(), cfg);
+    }
 
-	@Override
-	public void render() {
+    private static void setPanel(Panel panel) {
+        if (Kiosk.panel != null) Kiosk.panel.hide();
+        Kiosk.panel = panel;
+        if (Kiosk.panel != null) {
+            Kiosk.panel.show();
+            Kiosk.panel.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        }
+    }
 
-	}
+    public Panel getPanel() {
+        return panel;
+    }
 
-	@Override
-	public void pause() {
+    @Override
+    public void create() {
+        // Loads and configures the skin for the UI
+        AssetManager manager = new AssetManager();
+        manager.load("Graphics/tempSkin.json", Skin.class);
+        manager.finishLoading();
+        skin = manager.get("Graphics/tempSkin.json");
+        skin.getFont("font").setScale(.5f);
 
-	}
+        // Temporary test panel to make sure everything works
+        Panel test = new Panel() {
+            @Override
+            public void initialize() {
+                initialize("This is a test", "Can you read me?");
+            }
+        };
+        setPanel(test);
+    }
 
-	@Override
-	public void resume() {
+    @Override
+    public void resize(int width, int height) {
+        if (panel != null) panel.resize(width, height);
+    }
 
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (panel != null) panel.render(Gdx.graphics.getDeltaTime());
+    }
 
-	@Override
-	public void dispose() {
+    @Override
+    public void pause() {
+        if (panel != null) panel.pause();
+    }
 
-	}
+    @Override
+    public void resume() {
+        if (panel != null) panel.resume();
+    }
+
+    @Override
+    public void dispose() {
+        if (panel != null) panel.dispose();
+        if (skin != null) skin.dispose();
+    }
+
+    public void changePanels(Panel toPanel) {
+        panelHistory.add(getPanel());
+        // TODO Fade transition
+        setPanel(toPanel);
+    }
+
+    public static void back() {
+        if (!panelHistory.isEmpty()) setPanel(panelHistory.remove(panelHistory.size() - 1));
+    }
 }
--- src/com/thepaperpilot/kiosk/panels/Panel.java
+++ src/com/thepaperpilot/kiosk/panels/Panel.java
@@ -1,4 +1,97 @@
 package com.thepaperpilot.kiosk.panels;
 
-public interface Panel {
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.thepaperpilot.kiosk.Kiosk;
+
+public abstract class Panel implements Screen {
+
+    Label title;
+    Table body;
+    Stage stage;
+
+    public Panel() {
+        initialize();
+    }
+
+    public abstract void initialize();
+
+    public void initialize(String title, String bodyText) {
+        Table body = new Table(Kiosk.skin);
+        Label temp = new Label(bodyText, Kiosk.skin);
+        temp.setAlignment(Align.left);
+        body.top().add(temp).pad(Kiosk.PADDING);
+        initialize(title, body);
+    }
+
+    public void initialize(String title, Image bodyImage, String bodyText) {
+        Table body = new Table(Kiosk.skin);
+        body.add(bodyImage).width(Kiosk.WIDTH * .3f).fillY();
+        body.add(new Label(bodyText, Kiosk.skin)).fill();
+        initialize(title, body);
+    }
+
+    public void initialize(String titleText, Table body) {
+        this.body = body;
+
+        title = new Label(titleText, Kiosk.skin, "large");
+        title.setAlignment(Align.center);
+
+        Table panel = new Table(Kiosk.skin);
+        panel.setPosition(-Kiosk.WIDTH / 2 + Kiosk.PADDING, -Kiosk.HEIGHT / 2 + Kiosk.PADDING);
+        panel.setBackground(Kiosk.skin.getDrawable("blue_button07"));
+        panel.setSize(Kiosk.WIDTH - Kiosk.PADDING * 2, Kiosk.HEIGHT - Kiosk.PADDING * 2);
+        panel.top().add(title).fillX().padBottom(Kiosk.PADDING).row();
+        panel.top().add(body).expand().fill().pad(Kiosk.PADDING);
+
+        TextButton back = new TextButton("Back", Kiosk.skin, "large");
+        back.getLabel().setAlignment(Align.bottom);
+        back.setSize(120, 80);
+        back.setPosition(-Kiosk.WIDTH / 2 + Kiosk.PADDING * 2, Kiosk.HEIGHT / 2 - 70);
+        back.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                Kiosk.back();
+            }
+        });
+
+        stage = new Stage(new ScreenViewport());
+        stage.addActor(panel);
+        stage.addActor(back);
+    }
+
+    public void render(float delta) {
+        stage.draw();
+    }
+
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height);
+    }
+
+    public void show() {
+
+    }
+
+    public void hide() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void resume() {
+
+    }
+
+    public void dispose() {
+        stage.dispose();
+    }
 }
