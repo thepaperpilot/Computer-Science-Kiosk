11eba8668b48215c0708c851ac693d89a315ea14
TravelingGeek <gtl6645@gmail.com>
2014-06-11 01:49:47 -0500

    Time: 30m Tested launching Scratch from Kiosk application.

--- src/com/thepaperpilot/kiosk/panels/appControl.java
+++ src/com/thepaperpilot/kiosk/panels/appControl.java
@@ -1,16 +1,45 @@
 package com.thepaperpilot.kiosk.panels;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.thepaperpilot.kiosk.Kiosk;
+
+import java.io.IOException;
+
 /**
  * Created by Greg on 6/10/14.
  */
 public class appControl extends TopLevelPanel {
     @Override
     protected void initializeTL() {
-
+        initializeTL("Launch!", 1);
     }
 
     @Override
     public void initialize() {
 
+        Table body = new Table(Kiosk.skin);
+        body.top().add(new Label("Application Control", Kiosk.skin)).row();
+        TextButton subPanel = new TextButton("Launch Scratch", Kiosk.skin);
+        subPanel.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //Kiosk.changePanel(new tempPanel());
+                try {
+                    System.out.println("Opening Scratch");
+                    Runtime runTime = Runtime.getRuntime();
+                    Process process = runTime.exec("C:\\Program Files (x86)\\Scratch 2\\Scratch 2.exe");
+                    //System.out.println("Closing notepad");
+                    //process.destroy();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        body.add(subPanel);
+        initialize("This is Scratch", body);
     }
 }
