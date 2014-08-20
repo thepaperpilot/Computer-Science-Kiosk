cbb246f394751fa84c6b5701c5d9d6a1a7929e96
The Paper Pilot <thepaperpilot@gmail.com>
2014-08-20 16:44:02 -0500

    Time: 10m * Added subtitles to ScratchTut

--- src/com/thepaperpilot/kiosk/panels/ScratchTut.java
+++ src/com/thepaperpilot/kiosk/panels/ScratchTut.java
@@ -27,6 +27,9 @@ public class ScratchTut extends TopLevelPanel{
 		body.setWrap(true);
 		body.setAlignment(Align.center);
 
+		final Label title = new Label("General Scratch", Kiosk.skin, "large");
+		title.setAlignment(Align.center);
+
 		TextButton general = new TextButton("General Scratch", Kiosk.skin);
 		general.getLabel().setWrap(true);
 		general.addListener(new ClickListener() {
@@ -36,6 +39,7 @@ public class ScratchTut extends TopLevelPanel{
 						"Scratch is designed as an introduction to programing. It is quite simple to understand, doesn’t require " +
 						"the memorization needed for most programing, however still allows for creative “Hello World”-esque " +
 						"programs. Scratch helps people to think logically, systematically, and creatively, problem solve, etc.");
+				title.setText("General Scratch");
 			}
 		});
 		TextButton motion = new TextButton("Motion Blocks", Kiosk.skin);
@@ -46,6 +50,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 17 different types of motion blocks. Motion blocks are used to control how a sprite " +
 						"moves, such as sending it to a specified place, or moving it a specified amount in a given direction." +
 						"You can also use motion blocks to glide a sprite to a specified place over a specified amount of time.");
+				title.setText("Motion Blocks");
 			}
 		});
 		TextButton look = new TextButton("Look Blocks", Kiosk.skin);
@@ -56,6 +61,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 23 different types of looks blocks. Looks blocks control how a sprite looks, such as" +
 						"making a temporary or permanent thought or talk bubble, or controlling how big a sprite is, or even if " +
 						"you can see the sprite or not.");
+				title.setText("Look Blocks");
 			}
 		});
 		TextButton sound = new TextButton("Sound Blocks", Kiosk.skin);
@@ -66,6 +72,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 13 different types of sound blocks. Sound blocks control sounds and MIDI functions," +
 						"such as playing sounds, or playing sounds that pause the script. You can also use sound blocks to " +
 						"change the volume or tempo by a certain amount, or set the volume or tempo to a specific amount.");
+				title.setText("Sound Blocks");
 			}
 		});
 		TextButton pen = new TextButton("Pen Blocks", Kiosk.skin);
@@ -76,6 +83,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 11 different types of pen blocks. Pen blocks control the pen, such as changing or setting" +
 						"the pen size or shade amount. You can also use pen blocks to clear all the pen marks on screen or" +
 						"stamp the sprite's image onto the screen.");
+				title.setText("Pen Blocks");
 			}
 		});
 		TextButton data = new TextButton("Data Blocks", Kiosk.skin);
@@ -86,6 +94,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 15 different types of data blocks. There is two types of data blocks: variable and list " +
 						"blocks. Variable blocks hold and change values and strings. List blocks control lists, such as adding, " +
 						"deleting, and changing certain parts of a given list.");
+				title.setText("Data Blocks");
 			}
 		});
 		TextButton event = new TextButton("Event Blocks", Kiosk.skin);
@@ -97,6 +106,7 @@ public class ScratchTut extends TopLevelPanel{
 						"as activating certain scripts when the flag is pressed or activating certain scripts when a certain key is " +
 						"pressed. You can also use event blocks to activates scripts when the sprite is clicked or when there is a " +
 						"certain backdrop.");
+				title.setText("Event Blocks");
 			}
 		});
 		TextButton control = new TextButton("Control Blocks", Kiosk.skin);
@@ -107,6 +117,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 11 different types of control blocks. Control blocks allow you to control scripts, such as" +
 						"pausing a script for a certain amount of time or pausing a script until a given condition is true. They can " +
 						"also create loops that loop a certain amount of times or loops that continue forever.");
+				title.setText("Control Blocks");
 			}
 		});
 		TextButton sensing = new TextButton("Sensing Blocks", Kiosk.skin);
@@ -117,6 +128,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 20 different types of sensing blocks. Sensing blocks detect certain conditions, such as if" +
 						"a sprite is touching something else or if a certain key is being pressed. They can also detect the mouse's " +
 						"location and how many days it has been since 2000.");
+				title.setText("Sensing Blocks");
 			}
 		});
 		TextButton operator = new TextButton("Operator Blocks", Kiosk.skin);
@@ -127,6 +139,7 @@ public class ScratchTut extends TopLevelPanel{
 				body.setText("Scratch 2.0 has 17 different types of operator blocks. Operator blocks handle various math functions," +
 						"such as simple addition and subtraction or complex functions such as tangent, sine, and cosine. It can " +
 						"also pick a “random” number between two integers or round a value to the closest whole number.");
+				title.setText("Operator Blocks");
 			}
 		});
 		TextButton more = new TextButton("More Blocks", Kiosk.skin);
@@ -136,6 +149,7 @@ public class ScratchTut extends TopLevelPanel{
 			public void clicked(InputEvent event, float x, float y) {
 				body.setText("Scratch 2.0 has 2 different types of “more blocks”. More blocks are used to allow the user to make " +
 						"custom blocks. This is where you would input plugins if you were to use them.");
+				title.setText("More Blocks");
 			}
 		});
 		tabs.add(general).width(190).padBottom(5).row();
@@ -150,9 +164,13 @@ public class ScratchTut extends TopLevelPanel{
 		tabs.add(operator).width(190).padBottom(5).row();
 		tabs.add(more).width(190).padBottom(5).row();
 
+		Table right = new Table();
+		right.add(title).pad(10).row();
+		right.add(body).expandX().fillX();
+
 		panel.left();
 		panel.add(new ScrollPane(tabs, Kiosk.skin)).height(600).width(200);
-		panel.add(body).expandX().fillX();
+		panel.add(right).expand().fill();
 
 		initialize("How To Scratch", panel);
 	}
