13674765807721d9c2bd7667fdab171b292d6a74
The Paper Pilot <thepaperpilot@gmail.com>
2014-08-25 15:57:42 -0500

    Time: 5m * Made the resources panel look nicer

--- src/com/thepaperpilot/kiosk/panels/Resources.java
+++ src/com/thepaperpilot/kiosk/panels/Resources.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.thepaperpilot.kiosk.Kiosk;
 
 public class Resources extends TopLevelPanel {
@@ -15,12 +16,18 @@ public class Resources extends TopLevelPanel {
 	@Override
 	public void initialize() {
 		Table panel = new Table();
-		panel.add(new Label("Code.org", Kiosk.skin));
-		panel.add(new Image(Kiosk.manager.get("Graphics/code.png", Texture.class))).row();
-		panel.add(new Label("Codecademy.com", Kiosk.skin));
-		panel.add(new Image(Kiosk.manager.get("Graphics/codecademy.png", Texture.class))).row();
-		panel.add(new Label("Source Code for this Program", Kiosk.skin));
-		panel.add(new Image(Kiosk.manager.get("Graphics/github.png", Texture.class))).row();
+		Label code = new Label("Code.org", Kiosk.skin);
+		code.setAlignment(Align.right);
+		panel.add(code);
+		panel.add(new Image(Kiosk.manager.get("Graphics/code.png", Texture.class))).pad(10).row();
+		Label codecademy = new Label("Codecademy.com", Kiosk.skin);
+		codecademy.setAlignment(Align.right);
+		panel.add(codecademy);
+		panel.add(new Image(Kiosk.manager.get("Graphics/codecademy.png", Texture.class))).pad(10).row();
+		Label github = new Label("Source Code\nFor This Project", Kiosk.skin);
+		github.setAlignment(Align.right);
+		panel.add(github);
+		panel.add(new Image(Kiosk.manager.get("Graphics/github.png", Texture.class))).pad(10).row();
 
 		initialize("Online Resources", panel);
 	}
